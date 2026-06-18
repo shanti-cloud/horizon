88c26a58abdc20c9737ff9fbe27afc9746fb4594
Shanti <lorgana@yandex.ru>
2026-06-18 13:43:57 +0300

    add props

--- variables.ts
+++ variables.ts
@@ -6,17 +6,19 @@ export const LOCATION: YMapLocationRequest = {
     zoom: 8 // starting zoom
 };
 
-export const FIRST_MARKER_PROPS: {iconName: YMapDefaultMarkerProps['iconName']; title: YMapDefaultMarkerProps['title']; subtitle: YMapDefaultMarkerProps['subtitle']; coordinates: LngLat} = {
+export const FIRST_MARKER_PROPS: {iconName: YMapDefaultMarkerProps['iconName']; title: YMapDefaultMarkerProps['title']; color: YMapDefaultMarkerProps['color']; coordinates: LngLat} = {
     iconName: 'lindul',
 	title: "Линдуловская роща",
-    subtitle: "Экотропа среди вековых лиственниц и бурная река Рощинка.",
+    desc: "Экотропа среди вековых лиственниц и бурная река Рощинка.",
+	color: {day: '#2481cc', night: '#e8e85a'},
 	topicId: 428,
     coordinates: [29.5394, 60.2392]
 };
-export const SECOND_MARKER_PROPS: {iconName: YMapDefaultMarkerProps['iconName']; title: YMapDefaultMarkerProps['title']; subtitle: YMapDefaultMarkerProps['subtitle']; coordinates: LngLat} = {
+export const SECOND_MARKER_PROPS: {iconName: YMapDefaultMarkerProps['iconName']; title: YMapDefaultMarkerProps['title']; color: YMapDefaultMarkerProps['color']; coordinates: LngLat} = {
     iconName: 'vyborg',
 	title: "Выборгский замок",
-    subtitle: "Единственный в России полностью сохранившийся средневековый замок.",
+    desc: "Единственный в России полностью сохранившийся средневековый замок.",
+	color: {day: '#2481cc', night: '#e8e85a'},
 	topicId: 300,
     coordinates: [28.7291, 60.7158]
 };
