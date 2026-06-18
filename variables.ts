396c64190b0aa2d0d7219b4924eefb76d776ca2b
Shanti <lorgana@yandex.ru>
2026-06-18 13:38:50 +0300

    add props

--- variables.ts
+++ variables.ts
@@ -6,11 +6,17 @@ export const LOCATION: YMapLocationRequest = {
     zoom: 8 // starting zoom
 };
 
-export const FIRST_MARKER_PROPS: {iconName: YMapDefaultMarkerProps['iconName']; coordinates: LngLat} = {
+export const FIRST_MARKER_PROPS: {iconName: YMapDefaultMarkerProps['iconName']; title: YMapDefaultMarkerProps['title']; subtitle: YMapDefaultMarkerProps['subtitle']; coordinates: LngLat} = {
     iconName: 'lindul',
+	title: "Линдуловская роща",
+    subtitle: "Экотропа среди вековых лиственниц и бурная река Рощинка.",
+	topicId: 428,
     coordinates: [29.5394, 60.2392]
 };
-export const SECOND_MARKER_PROPS: {iconName: YMapDefaultMarkerProps['iconName']; coordinates: LngLat} = {
+export const SECOND_MARKER_PROPS: {iconName: YMapDefaultMarkerProps['iconName']; title: YMapDefaultMarkerProps['title']; subtitle: YMapDefaultMarkerProps['subtitle']; coordinates: LngLat} = {
     iconName: 'vyborg',
+	title: "Выборгский замок",
+    subtitle: "Единственный в России полностью сохранившийся средневековый замок.",
+	topicId: 300,
     coordinates: [28.7291, 60.7158]
 };
